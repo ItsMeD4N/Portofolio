df0a449797f63d923cb392c9f53a5c68d4626b8e
DAN <87706283+ItsMeD4N@users.noreply.github.com>
2025-10-26 21:47:13 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,15 @@ import Overview from "@/components/section/overview";
 import Projects from "@/components/section/projects";
 import { AnimatedThemeToggler } from "@/components/ui/animated-theme-toggler";
 
-
 export default function Home() {
   return (
-    <div className="relative" >
+    <div className="relative">
       <AnimatedThemeToggler className="fixed top-4 right-4 z-40 cursor-pointer" />
       <Intro />
       <SideNav>
         <div className="container mx-auto px-6 lg:px-0">
           <Overview />
           <Experience />
-          <Projects />
-          <Blogs />
         </div>
       </SideNav>
       <Contact />
